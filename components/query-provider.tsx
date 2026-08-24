e858014137f766105eb3c68f73ab2b1b7ea02651
camxus <camillus.konkwo@gmail.com>
2026-08-24 20:07:22 +0200

    fix staletime

--- components/query-provider.tsx
+++ components/query-provider.tsx
@@ -9,7 +9,7 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 60_000,
+            staleTime: 0,
             gcTime: 5 * 60_000,
             retry: 1,
             refetchOnWindowFocus: false,
